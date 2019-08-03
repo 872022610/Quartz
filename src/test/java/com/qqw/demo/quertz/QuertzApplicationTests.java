e8c7165ee439e694814591eb2bcafa2f6e1d9c77
quanqiwei <872022610@qq.com>
2019-08-03 16:45:55 +0800

    搭建项目环境，等待实现qurtz

---
+++ src/test/java/com/qqw/demo/quertz/QuertzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqw.demo.quertz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuertzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
